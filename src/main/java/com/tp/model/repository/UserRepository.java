e0a68227c703a113d10ffc95d64ca8ac4740b4b9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-14 22:22:28 +0100

    Update UserRepository.java

--- src/main/java/com/tp/model/repository/UserRepository.java
+++ src/main/java/com/tp/model/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.tp.model.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tp.model.entity.User;
@@ -13,5 +14,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 	List<User> findByActiveTrue(); 
 	List<User> findByActiveFalse();
 	List<User> findByNameOrderByNameDesc(String name);
+	@Query("Select max(age) from user")
+	User searchmaxage();
 
 }
