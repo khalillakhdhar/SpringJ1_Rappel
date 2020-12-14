ed90dea07fb72189773b544773fff4241b8f9884
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-14 10:12:25 +0100

    Update VoitureController.java

--- src/main/java/com/tp/Controller/VoitureController.java
+++ src/main/java/com/tp/Controller/VoitureController.java
@@ -1,5 +1,13 @@
 package com.tp.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VoitureController {
+@Autowired
+VoitureService vs;
+
+
 
 }
