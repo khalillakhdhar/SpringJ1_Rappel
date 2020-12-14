386c9070d5a3a49b4b22772badc7b7f9db2d53b4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-14 10:16:11 +0100

    Update VoitureController.java

--- src/main/java/com/tp/Controller/VoitureController.java
+++ src/main/java/com/tp/Controller/VoitureController.java
@@ -1,13 +1,36 @@
 package com.tp.Controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tp.model.entity.Voiture;
+
 @RestController
+@RequestMapping("car")
 public class VoitureController {
 @Autowired
 VoitureService vs;
 
+@PostMapping
+public Voiture savevt(@Valid @RequestBody Voiture voiture)
+{
+	
+return vs.ajouter(voiture);
+
+}
+@GetMapping
+public List<Voiture> affichage()
+{
+	return vs.lister();
 
 
+}
 }
